13bcb1b100ef9e183f1dcef518fcd1077486c6c5
PriyanshBordia <priyanshbordia1@gmail.com>
2021-11-28 17:48:31 +0530

    Update utility.py

--- utils/utility.py
+++ utils/utility.py
@@ -5,36 +5,36 @@ from .alerts import publish_on_telegram_channel
 
 
 def get_message(a):
-	message = {}
+	try:
+		message = {}
 
-	m = '<div class="container container-fluid text-center">'
-	m += '<h3>Dear Professor,</h3>'
+		m = '<div class="container container-fluid text-center">'
+		m += '<h3>Dear Professor,</h3>'
 
-	message['P'] = f'<div>{a.person.first_name} {a.person.last_name} wants to take a leave.</div>'
-	message['A'] = f'<div>You application dated <b>{a.created_at}</b> has been approved.</div>'
-	message['R'] = f'<div>You application dated <b>{a.created_at}</b> has been rejected by {a.up_next.get_role_display}.</div>'
+		message['P'] = f'<div>{a.person.first_name} {a.person.last_name} wants to take a leave.</div>'
+		message['A'] = f'<div>You application dated <b>{a.created_at}</b> has been approved.</div>'
+		message['R'] = f'<div>You application dated <b>{a.created_at}</b> has been rejected by {a.up_next.get_role_display}.</div>'
 
-	m += str(message[a.status])
-	m += '<h2>Application Details</h2>'
-	m += '<div>• From ' + str(a.start_date) + ' to ' + str(a.end_date) + '<br>'
-	m += '• Reason: ' + a.comments + '</div>'
-	m += '<footer>--<br>Regards<br>' + 'Team LMS</footer>'
-	m += '</div>'
+		m += str(message[a.status])
+		m += '<h2>Application Details</h2>'
+		m += '<div>• From ' + str(a.start_date) + ' to ' + str(a.end_date) + '<br>'
+		m += '• Reason: ' + a.comments + '</div>'
+		m += '<footer>--<br>Regards<br>' + 'Team LMS</footer>'
+		m += '</div>'
 
-	print(m)
-
-	template = '<html><head><style>div{font-size=1.4rem; line-height: 1.4rem;}</style></head><body>' + str(
-		m) + '</body></html>'
-	return template
+		template = '""<html><head><style>div{font-size=1.4rem; line-height: 1.4rem;}</style></head><body>' + str(m) + '</body></html>""'
+		return template
+	except Exception as e:
+		publish_on_telegram_channel(chat_id=int(os.getenv('TelegramChannel')), message=str(e))
 
 
 def send_application_mail(application):
-	# try:
-	subject = 'Leave Application'
-	message = get_message(application)
-	from_email = 'Team LNMIIT Leave Management System'
-	recipient_list = ['19uec117@lnmiit.ac.in', str(application.up_next.email)]
-	html_message = message
-	send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list, html_message=html_message)
-	# except Exception as e:
-		# publish_on_telegram_channel(chat_id=int(os.getenv('TelegramChannel')), message=str(e))
+	try:
+		subject = 'Leave Application'
+		message = get_message(application)
+		from_email = 'Team LNMIIT Leave Management System'
+		recipient_list = ['19uec117@lnmiit.ac.in', str(application.up_next.email)]
+		html_message = message
+		send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list, html_message=html_message)
+	except Exception as e:
+		publish_on_telegram_channel(chat_id=int(os.getenv('TelegramChannel')), message=str(e))
