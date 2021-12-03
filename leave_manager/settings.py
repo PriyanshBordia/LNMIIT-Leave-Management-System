4b519b153355bcb27c4311ba57241a00fd2e0441
PriyanshBordia <priyanshbordia1@gmail.com>
2021-12-03 17:05:04 +0530

    Update settings.py

--- leave_manager/settings.py
+++ leave_manager/settings.py
@@ -148,7 +148,7 @@ USE_TZ = False #True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
