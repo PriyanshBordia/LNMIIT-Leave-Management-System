ed0e02bc04bd89e19fe028928831b32dbe2367bc
PriyanshBordia <priyanshbordia1@gmail.com>
2021-12-07 17:14:37 +0530

    static

--- leave_manager/settings.py
+++ leave_manager/settings.py
@@ -151,10 +151,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static/'),
-    os.path.join(BASE_DIR, 'leave/static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, '/static/'),
+#     os.path.join(BASE_DIR, 'leave/static'),
+# )
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'leave/static/media')
