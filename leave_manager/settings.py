ebffcc66ce2e4515600a786c3c397f6543c9ea63
PriyanshBordia <priyanshbordia1@gmail.com>
2021-12-07 17:51:15 +0530

    Update settings.py

--- leave_manager/settings.py
+++ leave_manager/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   	# 'whitenoise.middleware.WhiteNoiseMiddleware',
+   	'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 WHITENOISE_AUTOREFRESH = True
