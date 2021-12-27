3c5f38004dd5f167abd7b554891e7507ae340915
PriyanshBordia <priyanshbordia1@gmail.com>
2021-12-27 16:18:40 +0530

    Added db_router

--- db_router.py
+++ db_router.py
@@ -7,7 +7,21 @@ class Router:
 	"""
 
 	def db_for_read(self, model, **hints):
-		return str(os.getenv('ENV'))
+		ENV = os.getenv('ENV')
+		if ENV is not None and ENV != '':
+			return str(os.getenv('ENV'))
+		else:
+			return 'default'
 
 	def db_for_write(self, model, **hints):
-		return str(os.getenv('ENV'))
+		ENV = os.getenv('ENV')
+		if ENV is not None and ENV != '':
+			return str(os.getenv('ENV'))
+		else:
+			return 'default'
+
+	def allow_relation(self, obj1, obj2, **hints):
+		pass
+
+	def allow_migrate(self, db, app_label, **hints):
+		pass
