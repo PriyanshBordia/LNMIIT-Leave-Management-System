cc31de3a2804afaf87f935e532800762d0dd5c07
PriyanshBordia <62763160+PriyanshBordia@users.noreply.github.com>
2022-10-30 00:31:34 +0530

    cleanup

--- leave_manager/urls.py
+++ leave_manager/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('leave.urls')),
     path('admin/', admin.site.urls),
+	path('', include('leave.urls')),
 	path('account/', include('accounts.urls')),
     path('account/', include('django.contrib.auth.urls')),
 ]
